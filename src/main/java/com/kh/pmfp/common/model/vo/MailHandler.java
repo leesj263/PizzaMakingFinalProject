daa69a62045cafbbf9796064dfebdcc1a36c5d62
yesol <yesol@DESKTOP-4INBCI3>
2019-02-11 19:34:12 +0900

    yesols

--- src/main/java/com/kh/pmfp/common/model/vo/MailHandler.java
+++ src/main/java/com/kh/pmfp/common/model/vo/MailHandler.java
@@ -1,51 +1,42 @@
 package com.kh.pmfp.common.model.vo;
-
 import java.io.UnsupportedEncodingException;
 
 import javax.activation.DataSource;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
-
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 
 public class MailHandler {
-	private JavaMailSender mailSender;
-	private MimeMessage message;
-	private MimeMessageHelper messageHelper;
-	
-	public MailHandler(JavaMailSender mailSender) throws MessagingException {
-		this.mailSender = mailSender;
-		message = this.mailSender.createMimeMessage();
-		messageHelper = new MimeMessageHelper(message, true, "UTF-8");
-	}
-	
-	public void setSubject(String subject) throws MessagingException {
-		messageHelper.setSubject(subject);
-		//이메일 타이틀
-	}
-	
-	public void setText(String htmlContent) throws MessagingException {
-		messageHelper.setText(htmlContent, true);
-		//이메일 text부분
-	}
 
-	public void setFrom(String email, String name) throws UnsupportedEncodingException, MessagingException {
-		messageHelper.setFrom(email, name);
-		//보내는 사람 이메일
-	}
+    private JavaMailSender mailSender;
+    private MimeMessage message;
+    private MimeMessageHelper messageHelper;
 
-	public void setTo(String email) throws MessagingException {
-		messageHelper.setTo(email);
-		//받는 사람 이메일
-	}
+    public MailHandler(JavaMailSender mailSender) throws MessagingException {
+        this.mailSender = mailSender;
+        message = this.mailSender.createMimeMessage();
+        messageHelper = new MimeMessageHelper(message, true, "UTF-8");
+    }
 
-	public void addInline(String contentId, DataSource dataSource) throws MessagingException {
-		messageHelper.addInline(contentId, dataSource);
-	}
+    public void setSubject(String subject) throws MessagingException {
+        messageHelper.setSubject(subject);
+    }
+    public void setText(String htmlContent) throws MessagingException {
+        messageHelper.setText(htmlContent, true);
+    }
+    public void setFrom(String email, String name) throws UnsupportedEncodingException, MessagingException {
+        messageHelper.setFrom(email, name);
+    }
+    public void setTo(String email) throws MessagingException {
+        messageHelper.setTo(email);
+    }
+    public void addInline(String contentId, DataSource dataSource) throws MessagingException {
+        messageHelper.addInline(contentId, dataSource);
+    }
+    public void send() {
+        mailSender.send(message);
+    }
 
-	public void send() {
-		mailSender.send(message);
-	}
 }
